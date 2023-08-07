d5498f3c7c814cd0a875c3846fe14b4c3395ad51
Cristian-000 <129860008+Cristian-000@users.noreply.github.com>
2023-08-07 13:08:18 -0300

    Agregamos listener al div y creamos el css

---
+++ eventos.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const contenedor = document.querySelector(".contenedor");
+  
+    contenedor.addEventListener("click", function() {
+      alert("Hola! Soy el div");
+    });
+  
+    function mostrarAlerta() {
+      alert("Hola!");
+    }
+});
+  
+  
+  
+  
+  
+  
