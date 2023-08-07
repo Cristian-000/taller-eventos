8902ccffe811ff1747e565fc8c9a39a630339561
Cristian-000 <129860008+Cristian-000@users.noreply.github.com>
2023-08-07 14:09:54 -0300

    añadimos la solucion de enfoque

--- eventos.js
+++ eventos.js
@@ -1,14 +1,40 @@
 document.addEventListener("DOMContentLoaded", function() {
     const contenedor = document.querySelector(".contenedor");
+    const boton = document.getElementById("saludarButton");
+  
+    contenedor.addEventListener("click", function(event) {
+        if (event.target === boton) {
+            mostrarAlerta();
+        } else if (event.target === contenedor) {
+            alert("Hola! Soy el div");
+        }
+    });
+  
+    function mostrarAlerta() {
+        alert("Hola!");
+    }
+});
+/*document.addEventListener("DOMContentLoaded", function() {
+    const contenedor = document.querySelector(".contenedor");
   
     contenedor.addEventListener("click", function() {
       alert("Hola! Soy el div");
     });
   
-    function mostrarAlerta() {
-      alert("Hola!");
+    function mostrarAlerta(event) {
+        if (event.target.id === "saludarButton") {
+            alert("Hola!");
+        }
     }
+
+    /*function mostrarAlerta(event) {
+    if (event.target.id === "saludarButton") {
+        alert("Hola!");
+    }
+    
+
 });
+*/
   
   
   
